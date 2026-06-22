84fc3f4a74a71cd75952a165978cfa69a28d72d1
Filip Tůma <tuma@asd-software.cz>
2026-06-22 13:49:48 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      { source: "/admin", destination: "/admin/index.html" },
-      { source: "/admin/", destination: "/admin/index.html" },
-    ];
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
